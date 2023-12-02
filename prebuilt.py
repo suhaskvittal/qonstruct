9f76fafd65c79eea9f7e1f7db65a0df84edbf58e
Suhas Vittal <suhaskvittal@gmail.com>
2023-12-01 20:17:05 -0500

    restructuring

--- prebuilt.py
+++ prebuilt.py
@@ -9,20 +9,21 @@ from asm.manager import AsmManager
 
 import qecolor
 
-def asm_hexagonal_color_code(output_file: str, distance: int, rounds: int, memory='z'):
+def asm_hexagonal_color_code(output_file: str, distance: int, rounds: int, memory='z', use_flags=True):
     code = qecolor.make_hexagonal_tanner_graph(distance)
     mgr = AsmManager(code, use_plaquettes_instead_of_checks=True)
     mgr.memory = memory
-    # Create flag qubits.
-    plaquettes = code.graph['plaquettes']
-    for plq in plaquettes:
-        support = code.graph['plaquette_support_map'][plq]
-        for i in range(0, len(support), 2):
-            q1 = support[i]
-            q2 = support[i+1]
-            if q1 is None or q2 is None:
-                continue
-            mgr.add_flags_to(q1, q2, plq)
+
+    if use_flags:
+        plaquettes = code.graph['plaquettes']
+        for plq in plaquettes:
+            support = code.graph['plaquette_support_map'][plq]
+            for i in range(0, len(support), 2):
+                q1 = support[i]
+                q2 = support[i+1]
+                if q1 is None or q2 is None:
+                    continue
+                mgr.add_flags_to(q1, q2, plq)
     # Write file.
     mgr.fopen(output_file)
     mgr.write_memory_experiment(rounds)
